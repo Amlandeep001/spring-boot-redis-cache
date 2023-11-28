d7516caad3c7c4c2f9124c082f9074ef98da189d
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2023-11-28 13:12:15 +0530

    Spring Boot version upgrade
    Spring Boot version upgrade

--- src/main/java/com/org/springboot/rediscache/controller/CustomerController.java
+++ src/main/java/com/org/springboot/rediscache/controller/CustomerController.java
@@ -1,7 +1,5 @@
 package com.org.springboot.rediscache.controller;
 
-import javax.annotation.Resource;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.org.springboot.rediscache.pojo.Customer;
 import com.org.springboot.rediscache.service.CustomerService;
+
+import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("/customers")
